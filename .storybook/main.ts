e01d120a642e77c25585e203831632223dc3e237
Hung Le <hung.le@optimziely.com>
2025-12-25 17:40:58 +0700

    Remove @storybook/addon-themes from Storybook configuration to streamline dependencies.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,8 +10,7 @@ const config: StorybookConfig = {
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-themes"
+    "@storybook/addon-onboarding"
   ],
   "framework": "@storybook/nextjs-vite",
   "staticDirs": [
